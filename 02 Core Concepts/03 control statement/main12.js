d1cee7b9ceea4273e47a37c527a63cb3cfb9b389
Daniel <71001476+kjh105702@users.noreply.github.com>
2022-01-01 23:09:40 +0900

    main12.js

---
+++ 02 Core Concepts/03 control statement/main12.js
@@ -0,0 +1,34 @@
+/*
+12. for문 더 연습하기
+
+실습과제
+주어진 높이(height)에 맞게 '*'로 삼각형을 그려주는 함수 printTriangle을 완성해 봅시다. 
+함수를 잘 작성했다면 결과는 아래와 같이 출력되어야 합니다.
+
+높이: 1
+*
+높이: 3
+*
+**
+***
+높이: 5
+*
+**
+***
+****
+*****
+*/
+
+function printTriangle(height) {
+	// 여기에 코드를 작성해 주세요.
+}
+
+// 테스트 코드
+console.log('높이: 1');
+printTriangle(1);
+
+console.log('높이: 3');
+printTriangle(3);
+
+console.log('높이: 5');
+printTriangle(5);
