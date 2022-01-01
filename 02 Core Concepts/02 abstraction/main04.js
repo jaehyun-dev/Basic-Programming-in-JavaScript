e2723d68f9461cd943580359da68a027bc3bae90
Daniel <71001476+kjh105702@users.noreply.github.com>
2022-01-01 20:10:51 +0900

    main04 solution

--- 02 Core Concepts/02 abstraction/main04.js
+++ 02 Core Concepts/02 abstraction/main04.js
@@ -11,8 +11,8 @@
 
 // 여기에 logParticipant 함수를 작성해 주세요.
 function logParticipant(name) {
-  console(`${name}(이)가 대화에 참여했습니다.`)
-}
+  console.log(`${name}(이)가 대화에 참여했습니다.`);
+};
 
 // 테스트 코드
 logParticipant('동수');
