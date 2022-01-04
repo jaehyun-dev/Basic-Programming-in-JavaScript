25eede21a636ad5e7ade853297520edd6bbd82d3
Daniel <71001476+kjh105702@users.noreply.github.com>
2022-01-04 19:31:06 +0900

    main04.js solution

--- 03 Data/02 array/main04.js
+++ 03 Data/02 array/main04.js
@@ -24,7 +24,14 @@ let celsiusTemps = [27, 25, 26, 22, 28, 27, 21];
 let fahrenheitTemps = [];
 
 // 여기에 코드를 작성해 주세요.
+function celsiusToFahrenheit(celsius) {
+  let fahrenheit = (celsius * 9 / 5) + 32;
+  return fahrenheit;
+}
 
+for (let i = 0; i < celsiusTemps.length; i++) {
+  fahrenheitTemps[i] = celsiusToFahrenheit(celsiusTemps[i]);
+}
 
 // fahrenheitTemps 테스트
 console.log(fahrenheitTemps);
