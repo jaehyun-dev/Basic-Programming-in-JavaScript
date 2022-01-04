14f1eb04db0bac55f60ee740710d92f56dd75b6b
Daniel <71001476+kjh105702@users.noreply.github.com>
2022-01-04 18:51:23 +0900

    main02.js

---
+++ 03 Data/02 array/main02.js
@@ -0,0 +1,22 @@
+/*
+02. 배열 인덱싱 연습하기
+
+실습과제
+let dataType = ['number', 'string', 'boolean', 'null', 'undefined', 'object'];
+여기, dataType 이라는 이름의 6개의 요소를 가진 배열이 있습니다.
+이 배열의 요소들을 모두 출력하는 프로그램을 작성해 보세요.
+반복문의 개념을 활용하시면 어렵지 않게 해결할 수 있습니다.
+
+코드를 잘 작성하셨다면, 다음과 같이 출력되어야 합니다.
+
+number
+string
+boolean
+null
+undefined
+object
+*/
+
+let dataType = ['number', 'string', 'boolean', 'null', 'undefined', 'object'];
+
+// 여기에 코드를 작성해 주세요.
