a738f99d0fd74f32b729e6bac2b28e760b047130
Daniel <71001476+kjh105702@users.noreply.github.com>
2022-01-04 17:11:56 +0900

    main06.js solution

--- 03 Data/01 object/main06.js
+++ 03 Data/01 object/main06.js
@@ -43,7 +43,9 @@ let myVoca = {
 };
 
 // 1. 이미 외운 단어 3개를 삭제해 주세요.
-// 여기에 코드를 작성해 주세요.
+delete myVoca.function;
+delete myVoca.constant;
+delete myVoca.local;
 
 
 console.log(myVoca);
@@ -51,7 +53,10 @@ console.log(myVoca.constant);
 
 
 // 2. 오늘 외울 단어 4개를 추가해 주세요.
-// 여기에 코드를 작성해 주세요.
+myVoca.extend = "확장하다";
+myVoca.export = "내보내다";
+myVoca.import = "불러오다";
+myVoca['default value'] = "기본값";
 
 
 console.log(myVoca);
@@ -59,4 +64,4 @@ console.log(myVoca.export);
 
 
 // 3. default value의 뜻을 출력해 주세요.
-// 여기에 코드를 작성해 주세요.
+console.log(myVoca["default value"])
