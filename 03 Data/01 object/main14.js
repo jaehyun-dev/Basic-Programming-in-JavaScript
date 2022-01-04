a2a324316c2dc99375b3eebfd0fc1bfd43867a09
Daniel <71001476+kjh105702@users.noreply.github.com>
2022-01-04 18:40:54 +0900

    main14.js solution

--- 03 Data/01 object/main14.js
+++ 03 Data/01 object/main14.js
@@ -18,7 +18,9 @@ let jaeSangStart = new Date(2109, 7, 1);
 
 function workDayCalc(startDate) {
 	// 여기에 코드를 작성해 주세요.
-	
+	let timeDiff = today.getTime() - startDate.getTime() 
+  let workDay = timeDiff / 1000 / 60 / 60 / 24 + 1
+  console.log(`오늘은 입사한 지 ${workDay}일째 되는 날 입니다.`)
 }
 
 workDayCalc(jaeSangStart);
