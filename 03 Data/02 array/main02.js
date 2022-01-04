0013aac779505ac886a37c52132855b65d99dabc
Daniel <71001476+kjh105702@users.noreply.github.com>
2022-01-04 18:56:55 +0900

    main02.js model solution

--- 03 Data/02 array/main02.js
+++ 03 Data/02 array/main02.js
@@ -20,6 +20,15 @@ object
 let dataType = ['number', 'string', 'boolean', 'null', 'undefined', 'object'];
 
 // 여기에 코드를 작성해 주세요.
+/*
 for (index in dataType) {
   console.log(dataType[index])
 }
+*/
+
+//모범답안
+for (let i = 0; i < 6; i++) {
+  console.log(dataType[i]);
+}
+
+//뒤에서 배우게 되지만 배열에서 for...in 문을 쓴느 것은 권장되지 않음.
