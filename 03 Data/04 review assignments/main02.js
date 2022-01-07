ede4741e412388e6dc7158a012bc24b8b992b501
Daniel <71001476+kjh105702@users.noreply.github.com>
2022-01-07 15:05:54 +0900

    main02.js model solution

--- 03 Data/04 review assignments/main02.js
+++ 03 Data/04 review assignments/main02.js
@@ -27,6 +27,7 @@
 1000원 지폐: 2장
 */
 
+/*
 function calculateChange(payment, cost) {
   // 코드를 작성해 주세요.
   let fiftyThousandBill = Math.floor((payment - cost) / 50000);
@@ -38,6 +39,29 @@ function calculateChange(payment, cost) {
   console.log(`10000원 지폐: ${tenThousandBill}장`);
   console.log(`5000원 지폐: ${fiveThousandBill}장`);
   console.log(`1000원 지폐: ${oneThousandBill}장`);
+}
+*/
+
+//모범 답안
+function calculateChange(payment, cost) {
+  // 코드를 작성해주세요.
+  let change = payment - cost;
+  
+  function billCounting(amount) {
+    const count = Math.floor(change / amount);
+    change = change - amount * count;
+    return count;
+  }
+
+  const fiftyCount = billCounting(50000)
+  const tenCount = billCounting(10000)
+  const fiveCount = billCounting(5000)
+  const oneCount = billCounting(1000)
+
+  console.log(`50000원 지폐: ${fiftyCount}장`);
+  console.log(`10000원 지폐: ${tenCount}장`);
+  console.log(`5000원 지폐: ${fiveCount}장`);
+  console.log(`1000원 지폐: ${oneCount}장`);
 }
 
 // 테스트 코드
